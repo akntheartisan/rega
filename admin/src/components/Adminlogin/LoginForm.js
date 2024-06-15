840ba77d5779a3a1bf1b84722c26105574afa490
akntheartisan <arvindnagaraj.n@gmail.com>
2024-06-15 19:48:33 +0530

    admin login frontend ok

--- admin/src/components/Adminlogin/LoginForm.js
+++ admin/src/components/Adminlogin/LoginForm.js
@@ -1,44 +1,55 @@
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
-import { TextField, Stack, InputAdornment,Button, Typography } from "@mui/material";
+import {
+  TextField,
+  Stack,
+  InputAdornment,
+  Button,
+  Typography,
+} from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import PasswordIcon from "@mui/icons-material/Password";
-import "./login.css"
+import "./login.css";
+import axios from "axios";
+import { client } from "../../Client/Clientaxios";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const submit = async ()=>{
+  const submit = async () => {
+    const credential = { username, password };
+
+    console.log(credential);
     try {
-
-      
-      
+      const response = await client.post("/regaadmin/signin", credential);
+      console.log(response);
     } catch (error) {
-      
+      console.log(error);
     }
-  }
+  };
 
   return (
     <>
       <div className="form">
-      <Box 
+        <Box
           sx={{
-            width: '300px',
-            height: '300px',
-            padding: '20px',
-            background: 'rgba(0,0,0,0.3)',
-            backdropFilter: 'blur(3px)',
-            borderRadius:'25px'
+            width: "300px",
+            height: "300px",
+            padding: "20px",
+            background: "rgba(0,0,0,0.3)",
+            backdropFilter: "blur(3px)",
+            borderRadius: "25px",
             // border:'1px ridge white'
           }}
         >
           <Stack direction={"column"} spacing={4}>
-            <Typography sx={{textAlign:'center'}}>Admin Login</Typography>
+            <Typography sx={{ textAlign: "center" }}>Admin Login</Typography>
             <TextField
               label="Username"
+              name="username"
               value={username}
-              onChange={(e)=>setUsername(e.target.value)}
+              onChange={(e) => setUsername(e.target.value)}
               variant="outlined"
               size="small"
               InputProps={{
@@ -51,8 +62,9 @@ const LoginForm = () => {
             />
             <TextField
               label="password"
+              name="password"
               value={password}
-              onChange={(e)=>setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
               variant="outlined"
               type="password"
               size="small"
@@ -64,7 +76,9 @@ const LoginForm = () => {
                 ),
               }}
             />
-            <Button size="small" variant="contained" onClick={submit}>LogIn</Button>
+            <Button size="small" variant="contained" onClick={submit}>
+              LogIn
+            </Button>
           </Stack>
         </Box>
       </div>
